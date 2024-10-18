d249f10acac2934940ceaacf19862cfb98925700
Gayatri <gayatrinikumbh12@gmail.com>
2024-10-18 19:19:11 +0530

    uu

--- src/test/java/piku/BaseTest.java
+++ src/test/java/piku/BaseTest.java
@@ -1,10 +1,14 @@
 package piku;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
-
+import static org.testng.Assert.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import utilities.PropertyUtils;
 
 public class BaseTest {
@@ -25,4 +29,14 @@ public class BaseTest {
         // Code to be executed after all tests
 		
     }
+	
+	@AfterTest
+	protected void assertExpectedStatusCode(int expectedStatusCode, Response response) {
+	    assertThat("Unexpected status code", response.getStatusCode(), equalTo(expectedStatusCode));
+	}
+
+	@AfterTest
+	protected void logResponse(Response response) {
+	    response.then().log().all();
+	}
 }
